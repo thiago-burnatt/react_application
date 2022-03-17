51ebd7e95c064bbf253c036585dd6ea66a312ae1
thiago-burnatt <thiago-burnatt@hotmail.com>
2022-03-17 14:30:10 -0300

    .

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'http://15.229.7.150',
-  baseURL: 'http:localhost:3001',
+  baseURL: 'http://15.229.7.150',
+  // baseURL: 'http:localhost:3001',
 });
